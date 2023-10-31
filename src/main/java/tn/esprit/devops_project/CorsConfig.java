24081065546704d305f4c29dcde2dec72f4c90c6
mehdiaroua <99272211+mehdiaroua@users.noreply.github.com>
2023-10-31 14:21:54 +0100

    Update CorsConfig.java

--- src/main/java/tn/esprit/devops_project/CorsConfig.java
+++ src/main/java/tn/esprit/devops_project/CorsConfig.java
@@ -1,2 +1,21 @@
-package tn.esprit.devops_project;public class CorsConfig {
+package tn.esprit.devops_project;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CorsConfig {
+   @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // This allows CORS for all endpoints
+                        .allowedOrigins("http://192.168.33.10:8083") // Replace with your Angular app's domain
+                        .allowedMethods("GET", "POST"); // Specify the HTTP methods you want to allow
+            }
+        };
+    }
 }
